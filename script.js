c22e225221c7faa026084d90a980c497dd9cc9c6
aKarthik25 <karthika2870@my.sduhsd.net>
2026-03-03 21:52:46 +0000

    Updated schedule.html, about.html, finish styling and javascript.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // add javascript here
+document.getElementById("myButton")?.addEventListener("click",() => {
+    document.getElementById("info").toggleAttribute("hidden");
+})
+document.getElementById("scheduleButton")?.addEventListener("click",() => {
+    day = document.getElementById("dayInput").value;
+
