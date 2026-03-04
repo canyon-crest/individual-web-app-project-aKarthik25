7fe2bb5d3706440970c1a65850b989271616f936
aKarthik25 <karthika2870@my.sduhsd.net>
2026-03-04 19:38:34 +0000

    Finished html, tinker styling and  JS

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 document.getElementById("myButton")?.addEventListener("click",() => {
     document.getElementById("info").toggleAttribute("hidden");
 })
-document.getElementById("scheduleButton")?.addEventListener("click",() => {
+document.getElementById("thisButton")?.addEventListener("click",() => {
+    const day = document.createElement("h1");
     day = document.getElementById("dayInput").value;
-
+    document.getElementById("scheduleList").toggleAttribute("hidden");
+})
